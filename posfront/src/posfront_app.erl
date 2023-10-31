ab0b62fb5f3cc73c15ba6aeb565dacea883ee651
dosmaster4life <jefferysuperman@gmail.com>
2023-10-31 17:17:29 -0600

    updates to fix frontend

--- posfront/src/posfront_app.erl
+++ posfront/src/posfront_app.erl
@@ -13,7 +13,7 @@ start(_Type, _Args) ->
 	Dispatch = cowboy_router:compile([
 	    {'_', [
 		%{"/", cowboy_static, {priv_file, db_access, "static/index.html"}},
-	        {"/", toppage_h, []},
+	     {"/", default_page_h, []},
 		{"/delivered",delivered_h,[]},
 		{"/pkg_loc",pkg_loc_h,[]},
 		{"/pkg_upd",pkg_upd_h,[]},
@@ -29,6 +29,13 @@ start(_Type, _Args) ->
 				{keyfile, PrivDir ++ "/ssl/privkey.pem"}
               		], #{env => #{dispatch => Dispatch}}),
 	posfront_sup:start_link().
+	
+	% cowboy:start_clear(
+    %     my_http_listener,
+    %     [{port, 8080}],
+    %     #{env => #{dispatch => Dispatch}}
+    % ).
+   % posfront_sup:start_link().
 
 
 stop(_State) ->
