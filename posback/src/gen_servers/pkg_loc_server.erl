49ea558822323306c2ed7f2dab7af7e07d957bc3
DiegoArrC <castanedadiego12.dac@gmail.com>
2023-11-09 14:48:44 -0700

    package_locate

--- posback/src/gen_servers/pkg_loc_server.erl
+++ posback/src/gen_servers/pkg_loc_server.erl
@@ -70,8 +70,8 @@ stop() -> gen_server:call(?MODULE, stop).
 package_locate(Package_data) ->
     % Tuple requires two parameters: function name and JSON data
     % JSON data is now a map
-    io:format("Package data: ~p~n",[Package_data]).
-    % gen_server:call({global,?MODULE}, {package_locate, Package_data}).
+    io:format("Package data: ~p~n",[Package_data]),
+    gen_server:call({global,?MODULE}, {package_locate, Package_data}).
 %%%===================================================================
 %%% gen_server callbacks
 %%%===================================================================
