7033f7255b2888604b22f6ee2bbe31b056d2360f
dosmaster4life <jefferysuperman@gmail.com>
2023-12-05 14:29:16 -0700

    logging

--- posfront/src/cowboy_handlers/delivered_h.erl
+++ posfront/src/cowboy_handlers/delivered_h.erl
@@ -17,6 +17,7 @@ init(Req0, Opts) ->
 call_mark_delivered(JSON) ->
 	% Send parsed JSON to back end
 	Backend_node = rpc:call('rrobin_serv@165.232.48.38', rrobin_serv, next, []),
+	io:format("Backend node: ~p~n", [Backend_node]),
 	rpc:cast(Backend_node, delivered_server, mark_delivered, [JSON]).
 	%io:format("Sent to back end~n", []).
 
